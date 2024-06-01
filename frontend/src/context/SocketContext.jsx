f2ac059ebc0ed66724c4c706e921b65e51ebf7de
chamindu77 <chamindunipun99@gmail.com>
2024-06-01 14:21:30 +0530

    update 1.1

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://real-time-chat-app-ehdj.onrender.com/login", {
+			const socket = io("https://real-time-chat-app-ehdj.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
